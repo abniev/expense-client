05c8d87e5badc352bfd758f21243b4aba256dffc
Abner Nieves <nieves.abner@gmail.com>
2024-07-09 16:20:09 -0400

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
 import "./App.css";
+import Graph from "./components/Graph";
+import Form from "./components/Form";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [] = useState(0);
 
   return (
     <div className="App">
-      <h1>Expense Tracker</h1>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
+        <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">
+          Expense Tracker
+        </h1>
+        {/*grid column*/}
+        <div className="grid md:grid-cols-2 gap-4">
+          {/*chart*/}
+          <Graph />
+          {/*form*/}
+          <Form />
+        </div>
+      </div>
     </div>
   );
 }
